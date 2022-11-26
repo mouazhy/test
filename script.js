8855dd12ecf05b5cb4bec64f98382f323a8f227e
mouaz <mouaz@gmail.com>
2022-11-26 14:54:17 +0200

    commit fille

---
+++ script.js
@@ -0,0 +1,45 @@
+document.querySelector('h1').style.color = 'red';
+
+
+let role = prompt('what your result???');
+
+if (role >= 90) { document.write('success') } else if (role >= 80) { document.write('very good') } else if (role >= 70) { document.write('good') } else if (role >= 60) { document.write('accept') } else { document.write('fail') }
+
+console.log(email.trim() == name);
+
+
+///////
+// = to put value into value 
+// == values
+// === values and datatype
+/////////
+// let email = prompt('enter your email!');
+// let name = 'best';
+// console.log(email.trim() == name);
+//trim()/ to remove spaces in input
+/////
+//concat()/ to merge arrray
+//join() / to convert fromm array to string
+//splice(start,count,add,add) ///delete
+//slice(start,end)  ////delete stay 
+/////
+//console.log(typeof String(1.0));
+// console.log(name.slice(2, 6)); ///st r
+// console.log(name.substring(2, 6)); ///st r
+// console.log(name.substr(2, 6)); ///st rid
+
+// console.log(name.includes('t', ));
+// console.log(name.startsWith('t', 3));
+// console.log(name.endsWith(' ', ));
+
+
+/////
+
+// let zakah = window.prompt();
+// console.log(0.010 * zakah + '$')
+///
+// console.log(+'5' - -'100');
+// console.log(parseInt('5.2'));
+// console.log(parseFloat('5.2'));
+// console.log(Math.abs(-5));
+// console.log(Math.floor(3.6));
